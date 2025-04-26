06ebbfc350e3bd85df64b70a208b4bf86188d2ce
Muhammad <Muhammed@YK-mc0119.local>
2025-04-26 20:59:43 +0200

    code update and skipping workflow to running [skip ci]

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import "./index.css";
 
 //This a comment
 // cooment from jec branch
+
+// another comment
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
